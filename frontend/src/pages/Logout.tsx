ff1dd334fa012becfce3e71570db2add6de92b84
lukaku3 <kazuhisa@lazyarea.com>
2025-05-04 18:27:23 +0900

    ログアウト時に全てのキャッシュ削除

--- frontend/src/pages/Logout.tsx
+++ frontend/src/pages/Logout.tsx
@@ -2,17 +2,21 @@ import { useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Logout() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     const doLogout = async () => {
       await signOut(auth);
+      queryClient.clear();
+      sessionStorage.clear();
       navigate("/login");
     };
     doLogout();
-  }, [navigate]);
+  }, [navigate, queryClient]);
 
   return null; // 何も表示しない
 }
