5ccb902993e7f257759b6e636d3f1a5d32c80b6c
lukaku3 <kazuhisa@lazyarea.com>
2025-05-07 15:33:31 +0900

    店舗一覧をCardへ入れるdraft

--- frontend/src/components/common/Card/BranchCard.tsx
+++ frontend/src/components/common/Card/BranchCard.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Branch } from "../../../types"; // 型定義ファイルのパスに合わせて修正
+import { useNavigate, useParams } from "react-router-dom";
 
 type BranchCardProps = {
   branch: Branch;
@@ -20,55 +22,45 @@ const bull = (
   </Box>
 );
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </React.Fragment>
-);
-
-// export default function OutlinedCard() {
-//   return (
-//     <Box sx={{ minWidth: 275 }}>
-//       <Card variant="outlined">{card}</Card>
-//     </Box>
-//   );
-// }
-
 const BranchCard: React.FC<BranchCardProps> = ({ branch }) => {
+  const navigate = useNavigate();
+  // パラメータ取得
+  const { company_id } = useParams<{ company_id: string }>();
+
   return (
     <Box sx={{ minWidth: 275 }}>
       <Card variant="outlined">
-        {/* {card} */}
         <React.Fragment>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {branch.branch_name}
-            </Typography>
-            <Typography sx={{ color: "text.secondary", mb: 0.5 }}>
-              TEL: {branch.phone}
-            </Typography>
-            <Typography variant="body2">
-              営業時間: {branch.business_hours}
-            </Typography>
-          </CardContent>
+          <CardActionArea
+            onClick={() =>
+              navigate(
+                `/company/${company_id}/branches/${branch.id}/reservations`
+              )
+            }
+          >
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {branch.branch_name}
+              </Typography>
+              <Typography sx={{ color: "text.secondary", mb: 0.5 }}>
+                TEL: {branch.phone}
+              </Typography>
+              <Typography variant="body2">
+                営業時間: {branch.business_hours}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{
+                  textAlign: "right",
+                  color: "primary.main",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                }}
+              >
+                予約へ
+              </Typography>
+            </CardContent>
+          </CardActionArea>
           <CardActions>
             <Button size="small">店舗詳細</Button>
           </CardActions>
